a9a844d3388cff52fe63bcb5ed3184259e19489e
salim <jimsky699@gmail.com>
2023-03-23 16:52:46 +0100

    change emailjs settings

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -3,26 +3,26 @@ const contactForm = document.getElementById('contact-form')
 // console.log(contactForm)
 
 
-contactForm.addEventListener('submit', function(e) {
+contactForm.addEventListener('submit', function (e) {
 
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const message = document.getElementById('message').value
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  const message = document.getElementById('message').value
 
-    e.preventDefault()
-    
-    try {
-      emailjs.send("service_4m625gs","template_l5u6yuk",{
-        name: name,
-        from_name: email,
-        message: message,
-      })
+  e.preventDefault()
+
+  try {
+    emailjs.send("service_snqenkg", "template_29fkhbj", {
+      name: name,
+      from_name: email,
+      message: message,
+    })
       .then(() => {
         alert('email envoyer !')
       }, (err) => {
         alert('un Probleme est survenue recomencer !')
       })
-    } catch (error) {
-      alert('Connexion a l\'api impossible !')
-    }
+  } catch (error) {
+    alert('Connexion a l\'api impossible !')
+  }
 })
